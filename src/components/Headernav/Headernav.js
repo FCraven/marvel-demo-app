fdf6b1214285817f02217df9703287794624cb79
FCraven <craven.francis@gmail.com>
2019-12-30 17:53:04 -0500

    Header and headernav good for now. Start work on body-content and redux tomorrow.

--- src/components/Headernav/Headernav.js
+++ src/components/Headernav/Headernav.js
@@ -3,16 +3,18 @@ import './Headernav.css'
 
 const Headernav =(props)=> {
   return (
+
     <nav id='headernav'>
       <ul id='headernav-navlist'>
-        <li className='headernav-link'>Creators</li>
-        <li className='headernav-link'>Series</li>
-        <li className='headernav-link'>Stories</li>
-        <li className='headernav-link'>Comics</li>
         <li className='headernav-link'>Characters</li>
+        <li className='headernav-link'>Comics</li>
         <li className='headernav-link'>Events</li>
+        <li className='headernav-link'>Stories</li>
+        <li className='headernav-link'>Series</li>
+        <li className='headernav-link'>Creators</li>
       </ul>
     </nav>
+
   )
 }
 
