5c8159883d95b0412916c2d6a50dd44a2ee07313
FCraven <craven.francis@gmail.com>
2019-12-30 11:47:07 -0500

    moved component info to aprop loc

---
+++ src/components/Characters/Characters.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Characters.css'
+
+const Characters = (props) => {
+  return (
+    <section id='characters'>
+
+      <h1>Characters</h1>
+
+
+    </section>
+  )
+}
+
+
+export default Characters;
