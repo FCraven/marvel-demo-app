0c1795af87605fea764a302a856c46ef10e6b5a7
FCraven <craven.francis@gmail.com>
2019-12-30 21:00:52 -0500

    connect components in the morning

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import './Footer.css'
 const Footer =(props)=> {
   return (
     <footer id="footer">
+        <h4>Excelsior!</h4>
         <a href="http://marvel.com">Data provided by Marvel. © 2019 MARVEL</a>
     </footer>
   )
