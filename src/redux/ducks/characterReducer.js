e37ce36864c174f3b3fd8e7127edcd38de921f1a
FCraven <craven.francis@gmail.com>
2020-02-07 12:51:01 -0500

    change key var

--- src/redux/ducks/characterReducer.js
+++ src/redux/ducks/characterReducer.js
@@ -7,7 +7,7 @@ export const initialState = {
   characters: [],
 }
 
-const { MARVEL_API_PUBLIC_KEY } = process.env
+const MARVEL_API_PUBLIC_KEY = process.env.MARVEL_API_PUBLIC_KEY
 
 //Actions
 export const FETCH_INITIAL_CHARACTERS_BY_LETTER = 'FETCH_INITIAL_CHARACTERS_BY_LETTER'
