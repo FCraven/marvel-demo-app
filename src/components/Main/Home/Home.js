a39555938c204b8e71d316e0a3e1baacec465a2f
FCraven <craven.francis@gmail.com>
2020-02-07 13:43:13 -0500

    getting annoyed

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Home.css'
 
 const Home =(props)=> {
-console.log(`PROCESS.ENV-->`, process.env)
+console.log(`PROCESS.ENV.KEY-->`, process.env.MARVEL_API_PUBLIC_KEY)
   return (
     <section id='home-component' className='flex-container-center'>
       <div id='home-title' className='flex-container-center'>
