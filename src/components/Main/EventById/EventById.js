1fe18a398c957f6a091a2baeb99aad2cfb537d4d
FCraven <craven.francis@gmail.com>
2020-02-13 12:00:15 -0500

    event by id css added and working on layout

--- src/components/Main/EventById/EventById.js
+++ src/components/Main/EventById/EventById.js
@@ -16,17 +16,42 @@ class EventById extends Component {
   render() {
     const { title, description, thumbnail } = this.props
 
+    const imgTileStyle = {
+      position: 'relative',
+      display: 'flex',
+      backgroundImage: `url(${thumbnail.path}/landscape_incredible.${thumbnail.extension})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: '100% 100%',
+      width: '100%',
+      maxWidth: '100%',
+      height: '100%'
+      }
+
+      const opacityLayer = {
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        backgroundColor: 'black',
+        background: 'radial-gradient(circle, rgba(0,0,0,0.4192051820728291) 0%, rgba(0,0,0,0.7525385154061625) 43%, rgba(0,0,0,0.9037990196078431) 90%, rgba(0,0,0,0.9430147058823529) 100%)',
+        overflowX: 'auto',
+        overflowY: 'auto'
+      }
+
     console.log(`EVTBYIDPROPS--->`, this.props)
     return (
-      <div style={{display: 'inline-flex'}}>
-        <img src={`${thumbnail.path}/landscape_incredible.${thumbnail.extension}`} />
-        <div>
-          <h1>{title}</h1>
-          <p>{description}</p>
+
+      <article id='event-by-id-container'>
+        <div style={imgTileStyle}>
+          <div style={opacityLayer}>
+           <h3>{title}</h3>
+
+          </div>
         </div>
         <div>Map out Character Tiles here</div>
         <div>Map out Comics Tile here</div>
-      </div>
+
+      </article>
+
     )
   }
 }
