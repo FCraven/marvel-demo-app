ab7ab5d4ba8d986bc9e6887e95ba959646d5037f
FCraven <craven.francis@gmail.com>
2020-02-25 15:27:46 -0500

    hero component with image and arrows added to evt by id component

--- src/components/Main/EventById/EventById.js
+++ src/components/Main/EventById/EventById.js
@@ -36,7 +36,7 @@ class EventById extends Component {
       // display: 'flex',
       // alignItems: 'flex-end',
       // justifyContent: 'flex-end',
-      backgroundColor: 'black',
+      backgroundColor: 'midnightblue',
       background: 'radial-gradient(circle, rgba(0,0,0,0.4192051820728291) 0%, rgba(0,0,0,0.7525385154061625) 43%, rgba(0,0,0,0.9037990196078431) 90%, rgba(0,0,0,0.9430147058823529) 100%)',
       // overflowX: 'auto',
       // overflowY: 'auto'
@@ -45,18 +45,32 @@ class EventById extends Component {
     console.log(`EVTBYIDPROPS--->`, this.props)
     return (
       <article id='event-by-id-container' style={opacityLayer}>
-        <img id='event-by-id-image'
-          src={`${thumbnail.path}/landscape_incredible.${thumbnail.extension}`}
-          alt="Event thumbnail" />
+        <div id='hero-component'>
+          <FontAwesomeIcon id='home-icon'
+            className='arrows'
+            icon={faCaretLeft}
+            size='5x'
+            style={{
+              color: '#EA2328',
+              padding: '0 5px'
+            }} />
+          <img id='event-by-id-image'
+            src={`${thumbnail.path}/landscape_incredible.${thumbnail.extension}`}
+            alt="Event thumbnail" />
+          <FontAwesomeIcon id='home-icon'
+            className='arrows'
+            icon={faCaretRight}
+            size='5x'
+            style={{
+              color: '#EA2328',
+              padding: '0 5px'
+            }} />
+        </div>
         <h3 id='event-by-id-title'>{title}</h3>
         <p id='event-by-id-text'>{description}</p>
         <div id='event-by-id-buttons'>
-          <button style={{ height: '20px', width: '50px', backgroundColor: 'hotpink' }}>Characters</button>
-          <button style={{  height: '20px', width: '50px', backgroundColor: 'hotpink' }}>Comics</button>
-        </div>
-        <div>
-          <FontAwesomeIcon id='home-icon' icon={faCaretLeft} size='3x' style={{ color: '#EA2328', padding: '0 5px' }} />
-          <FontAwesomeIcon id='home-icon' icon={faCaretRight} size='3x' style={{ color: '#EA2328', padding: '0 5px' }} />
+          <button type='button' className='event-by-id-button'>Characters</button>
+          <button type='button' className='event-by-id-button'>Comics</button>
         </div>
       </article>
     )
--- src/components/Sidenav/Sidenav.js
+++ src/components/Sidenav/Sidenav.js
@@ -6,6 +6,8 @@ import CharacterSearchBar from './CharacterSearchBar'
 import ByIdSidenav from './ByIdSidenav'
 
 const Sidenav =(props)=> {
+  // add button to open/ close
+
   return (
     <aside id='sidenav' className='flex-container-center'>
         <Switch>
