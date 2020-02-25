b315c3331112d3340e11968fb8d2ff317ae3dd06
FCraven <craven.francis@gmail.com>
2020-02-25 13:52:14 -0500

    style for eventbyid card shaping up

--- src/components/Main/EventById/EventById.js
+++ src/components/Main/EventById/EventById.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import './EventById.css'
 import { fetchEventInfo } from '../../../redux/ducks/selectedEventReducer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretRight, faCaretLeft } from '@fortawesome/free-solid-svg-icons'
+
 
 class EventById extends Component {
   constructor(props) {
@@ -25,33 +28,37 @@ class EventById extends Component {
       width: '100%',
       maxWidth: '100%',
       height: '100%'
-      }
+    }
 
-      const opacityLayer = {
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'flex-end',
-        justifyContent: 'flex-end',
-        backgroundColor: 'black',
-        background: 'radial-gradient(circle, rgba(0,0,0,0.4192051820728291) 0%, rgba(0,0,0,0.7525385154061625) 43%, rgba(0,0,0,0.9037990196078431) 90%, rgba(0,0,0,0.9430147058823529) 100%)',
-        overflowX: 'auto',
-        overflowY: 'auto'
-      }
+    const opacityLayer = {
+      // height: '100%',
+      // width: '100%',
+      // display: 'flex',
+      // alignItems: 'flex-end',
+      // justifyContent: 'flex-end',
+      backgroundColor: 'black',
+      background: 'radial-gradient(circle, rgba(0,0,0,0.4192051820728291) 0%, rgba(0,0,0,0.7525385154061625) 43%, rgba(0,0,0,0.9037990196078431) 90%, rgba(0,0,0,0.9430147058823529) 100%)',
+      // overflowX: 'auto',
+      // overflowY: 'auto'
+    }
 
     console.log(`EVTBYIDPROPS--->`, this.props)
     return (
-
-      <article id='event-by-id-container'>
-        <div style={imgTileStyle}>
-          <div style={opacityLayer}>
-           <h3 id='event-by-id-text'>EVENT:  {title}</h3>
-          </div>
+      <article id='event-by-id-container' style={opacityLayer}>
+        <img id='event-by-id-image'
+          src={`${thumbnail.path}/landscape_incredible.${thumbnail.extension}`}
+          alt="Event thumbnail" />
+        <h3 id='event-by-id-title'>{title}</h3>
+        <p id='event-by-id-text'>{description}</p>
+        <div id='event-by-id-buttons'>
+          <button style={{ height: '20px', width: '50px', backgroundColor: 'hotpink' }}>Characters</button>
+          <button style={{  height: '20px', width: '50px', backgroundColor: 'hotpink' }}>Comics</button>
         </div>
-        <div>Map out Character Tiles here</div>
-        <div>Map out Comics Tile here</div>
+        <div>
+          <FontAwesomeIcon id='home-icon' icon={faCaretLeft} size='3x' style={{ color: '#EA2328', padding: '0 5px' }} />
+          <FontAwesomeIcon id='home-icon' icon={faCaretRight} size='3x' style={{ color: '#EA2328', padding: '0 5px' }} />
+        </div>
       </article>
-
     )
   }
 }
