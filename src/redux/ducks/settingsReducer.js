e638352f388cf1cbc42a645d4b48c0c5e09ac17b
FCraven <craven.francis@gmail.com>
2020-02-27 12:03:56 -0500

    built

--- src/redux/ducks/settingsReducer.js
+++ src/redux/ducks/settingsReducer.js
@@ -1,25 +1,25 @@
-export const initialState= {
+export const initialState = {
   isLoading: false,
-  isSearchOpen: false
+  isSearchOpen: true
 }
 
 export const TOGGLE_LOADING = 'TOGGLE_LOADING'
 export const TOGGLE_SEARCH = 'TOGGLE_SEARCH'
 
-export const toggleLoading =()=> {
+export const toggleLoading = () => {
   return {
     type: TOGGLE_LOADING
   }
 }
 
-export const toggleSearch =()=> {
+export const toggleSearch = () => {
   return {
     type: TOGGLE_SEARCH
   }
 }
 
-const settingsReducer =(state = initialState, action)=> {
-  switch(action.type) {
+const settingsReducer = (state = initialState, action) => {
+  switch (action.type) {
     case TOGGLE_LOADING:
       return Object.assign({}, state, {
         isLoading: !state.isLoading
@@ -29,7 +29,7 @@ const settingsReducer =(state = initialState, action)=> {
       return Object.assign({}, state, {
         isSearchOpen: !state.isSearchOpen
       })
-      
+
     default:
       return state;
   }
