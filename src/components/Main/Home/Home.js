7b1700f66926d78910d91b3a588c5cc4f30fc445
FCraven <craven.francis@gmail.com>
2020-02-28 14:49:14 -0500

    home title under construction

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import './Home.css'
 
-const Home =(props)=> {
+const Home = (props) => {
 
   return (
     <section id='home-component' className='flex-container-center'>
       <div id='home-title' className='flex-container-center'>
-          <div id='home-opacity-screen'>
-            
-          </div>
+        <div id='home-opacity-screen'>
+
+          <h1>{`Welcome!
+            Please feel free to browse
+            around while the page is app
+            is under construction!`}</h1>
+        </div>
 
       </div>
     </section>
